1e0628f22a5935785fc4f6f0ef215518ae59738c
Nguyenphu1807 <Nguyenphuyh2812@users.noreply.github.com>
2020-10-16 11:26:55 +0700

    test

---
+++ Practice_dotnet/Practice_dotnet/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Practice_dotnet.Models;
+using Practice_dotnet.Services;
+
+namespace Practice_dotnet.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        //[HttpPath] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] String ProductId,
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
---
+++ Practice_dotnet/Practice_dotnet/Models/product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Practice_dotnet.Models
+{
+    public class product
+    {
+        public string Id { get; set; }
+
+        public string name { get; set; }
+
+        public string price { get; set; }
+
+
+        public string Url { get; set; }
+
+        public int[] Ratings { get; set; }
+
+        public override String ToString() => JsonSerializer.Serialize<product>(this);
+
+    }
+}
